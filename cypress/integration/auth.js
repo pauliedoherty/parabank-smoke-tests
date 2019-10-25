5802d44f264fa52847f38f7f502da953424bb8b5
pauliedoherty <paul.doherty03@gmail.com>
2019-10-25 00:11:00 -0700

    Login tests added

--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -1,23 +1,18 @@
 describe('Auth', function() {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/'); // visit baseUrl
   })
   it('Checks valid user can login', function() {
     cy.fixture('logins.json').then((user) => {
-      cy.get('form').within(() => {
-        cy.get('input[name="username"]').type(`${user.username}`);
-        cy.get('input[name="password"]').type(`${user.password}`);
-        cy.get('.button').click();
-        cy.url().should('eq', 'https://parabank.parasoft.com/parabank/overview.htm');
-      });
+      cy.loginUI(user.validUser);
     })
   });
 
   it('Checks wrong credentials cannot login', function() {
-
+    cy.url().should('eq', 'https://parabank.parasoft.com/parabank/overview.htm');
   });
 
   it('Checks correct account is loaded', function() {
-
+    cy.url().should('eq', 'https://parabank.parasoft.com/parabank/overview.htm');
   });
 });

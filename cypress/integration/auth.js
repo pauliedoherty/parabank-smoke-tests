a6e32bbf141ee866702446bce19d417fedfb13fc
pauliedoherty <paul.doherty03@gmail.com>
2019-10-27 23:31:56 -0700

    Login form tests

--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -1,18 +1,24 @@
 describe('Auth', function() {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/'); // visit baseUrl
   })
+  after(() => {
+    cy.clearCookies();
+  }); 
   it('Checks valid user can login', function() {
     cy.fixture('logins.json').then((user) => {
-      cy.loginUI(user.validUser);
+      cy.loginUI(user.myAccount);
     })
   });
 
   it('Checks wrong credentials cannot login', function() {
-    cy.url().should('eq', 'https://parabank.parasoft.com/parabank/overview.htm');
+    cy.get('form').within(() => {
+      cy.get('.button').click();
+    });
+    cy.get('.title').should('have.text', 'Error!');
   });
 
-  it('Checks correct account is loaded', function() {
-    cy.url().should('eq', 'https://parabank.parasoft.com/parabank/overview.htm');
-  });
+  // it('Checks correct account is loaded', function() {
+      // I removed this test because there were cases logging in would navigate to a different
+  // });
 });

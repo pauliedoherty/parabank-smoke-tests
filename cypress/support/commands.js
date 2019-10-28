99196c3f74e6e3f309b8076de1bb4051e1b9303c
pauliedoherty <paul.doherty03@gmail.com>
2019-10-27 23:33:58 -0700

    Cypress macros added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,19 @@ Cypress.Commands.add("loginUI", (user) => {
     cy.get('input[name="username"]').type(`${user.username}`);
     cy.get('input[name="password"]').type(`${user.password}`);
     cy.get('.button').click();
+    cy.wait(1000);
   });
 });
 
+Cypress.Commands.add("checkLatestAccount", (latestAccount) => {
+  cy.get('[id="leftPanel"]').contains('Accounts Overview').click();
+  cy.wait(1000);
+  cy.get('div[id="rightPanel"]').contains('Accounts Overview');
+  cy.get('table[id="accountTable"] tr').eq(-3).should('contain', latestAccount); 
+  cy.get('table[id="accountTable"] tr').eq(-3).contains(latestAccount).click();
+  cy.wait(1000);
+  cy.log('Check Account URL')
+  cy.url().should('eq', 'https://parabank.parasoft.com/parabank/activity.htm?id=' + latestAccount);
+  cy.log('Check correct account loaded in Account Details form');
+  cy.get('table').contains('td[id="accountId"]', latestAccount);  
+});  
